d1df86c6f3c519462f2147b75220c04325745543
Никита Комаров <nikita.komarov01.12.92@gmail.com>
2022-04-03 17:39:38 +0300

    3try

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -11,7 +11,10 @@ urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
 ]
 
+handler500 = 'core.views.server_error'
 handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.permission_denied'
+
 
 if settings.DEBUG:
     urlpatterns += static(
